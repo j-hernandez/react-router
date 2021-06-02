33650169163ae8d783731843a6e8ba0ce0735918
James Hernandez <james@j-hernandez.com>
2021-06-01 21:08:00 -0400

    adds router

--- src/App.js
+++ src/App.js
@@ -4,12 +4,37 @@ import Home from './components/Home';
 import Blog from './components/Blog';
 import About from './components/About';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 function App() {
   return (
       <div className="App">
-          <Home />
-          <Blog />
-          <About />
+        <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/blog">Blog</Link>
+            <Link to="/about">About</Link>
+          </nav>
+        </div>
+
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/blog">
+              <Blog />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </Router>
       </div>
   );
 }
